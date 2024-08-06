3e565940096501cf1f913daa0c61cd18d74d949f
zeynepinanc <zeynep.inanc@gmail.com>
2024-08-06 16:22:29 +0300

    Sign up page edit

--- src/Pages/SignupModal.jsx
+++ src/Pages/SignupModal.jsx
@@ -7,20 +7,42 @@ function SignupModal({ show, handleClose }) {
       <div className="modal-dialog modal-dialog-centered" role="document">
         <div className="modal-content bg-black text-white">
           <div className="modal-header bg-black border-0">
-            <h5 className="modal-title text-white">Hesabını oluştur</h5>
-            <button type="button" className="close text-white ml-auto" aria-label="Close" onClick={handleClose}>
+            <h5 className="modal-title text-white">Hesap Oluştur</h5>
+            <button type="button" className=" position-absolute top-0 end-0 m-3" aria-label="Close" onClick={handleClose}>
               <span aria-hidden="true">&times;</span>
             </button>
           </div>
           <div className="modal-body bg-black text-white">
             <form>
               <div className="form-group mb-3">
-                <label className="text-danger">İsim</label>
-                <input type="text" className="form-control" placeholder="İsim" maxLength="50" />
+                <label className="text-danger">Ad-Soyad</label>
+                <input type="text" className="form-control" placeholder="Ad-Soyad giriniz" maxLength="50" />
               </div>
               <div className="form-group mb-3">
                 <label className="text-danger">E-posta</label>
-                <input type="email" className="form-control" placeholder="E-posta" />
+                <input type="email" className="form-control" placeholder="E-postanızı giriniz" />
+              </div>
+              <div className="form-group mb-3">
+                <label className="text-danger">Sicil Numarası</label>
+                <input type="text" className="form-control" placeholder="Sicil Numaranızı giriniz" maxLength="50" />
+              </div>
+              <div className="form-group mb-3">
+                <label className="text-danger">Bölüm</label>
+                <select className="form-control">
+                  <option value="">Bölümünüzü Seçiniz</option>
+                  <option value="b1">Bölüm 1</option>
+                  <option value="b2">Bölüm 2</option>
+                  <option value="b3">Bölüm 3</option></select>
+                
+              </div>
+              <div className="form-group mb-3">
+                <label className="text-danger">Müdürlük</label>
+                <select className="form-control">
+                  <option value="">Müdürlüğünüzü Seçiniz</option>
+                  <option value="m1">Müdürlük 1</option>
+                  <option value="m2">Müdürlük 2</option>
+                  <option value="m3">Müdürlük 3</option>
+                </select>
               </div>
               <div className="form-group mb-3">
                 <label className="text-danger">Şifre</label>
